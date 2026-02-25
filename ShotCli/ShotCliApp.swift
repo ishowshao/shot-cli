29ba1e4264fffad21bbc6681ab612d5557ee8b90
ishowshao <234047485@qq.com>
2026-02-25 17:34:47 +0800

    Initial commit

---
+++ ShotCli/ShotCliApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotCliApp.swift
+//  ShotCli
+//
+//  Created by cyberoldman on 2026/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ShotCliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
